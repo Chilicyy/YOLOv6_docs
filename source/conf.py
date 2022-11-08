796522671854b9838fcf25ec99f2cdb3b91a6ee5
Chilicyy <lichuyi@meituan.com>
2022-11-08 15:08:24 +0800

    update

--- source/conf.py
+++ source/conf.py
@@ -11,13 +11,16 @@ project = 'YOLOv6_docs'
 copyright = '2022, meituan'
 author = 'meituan'
 
+release = '1.0'
+version = '1.0.0'
+
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 
 
-templates_path = ['_templates']
-exclude_patterns = []
+# templates_path = ['_templates']
+# exclude_patterns = []
 
 language = 'zh_CN'
 
@@ -32,14 +35,47 @@ language = 'zh_CN'
 # html_theme = 'sphinx_bernard_theme'
 # html_theme_path = [sphinx_bernard_theme.get_html_theme_path()]
 
-html_static_path = ['_static']
-html_logo = "_static/yolov6_logo.png"
-#html_theme_path = [sphinx_bernard_theme.get_html_theme_path()]
+# html_static_path = ['_static']
+# html_logo = "_static/yolov6_logo.png"
+# #html_theme_path = [sphinx_bernard_theme.get_html_theme_path()]
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+# html_theme = 'alabaster'
+# html_static_path = ['_static']
+
+# extensions = [
+#      'recommonmark',
+#      'sphinx_markdown_tables'
+# ]
+
+# -- General configuration
 
 extensions = [
-     'recommonmark',
-     'sphinx_markdown_tables'
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.intersphinx',
+    'recommonmark',
+    'sphinx_markdown_tables',
 ]
+
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+}
+intersphinx_disabled_domains = ['std']
+
+templates_path = ['_templates']
+
+# -- Options for HTML output
+html_theme = 'sphinx_rtd_theme'
+
+# -- Options for EPUB output
+epub_show_urls = 'footnote'
+
+# -- Options for PDF output
+latex_engine = 'xelatex'
+latex_use_xindy = False
+latex_elements = {
+    'preamble': '\\usepackage[UTF8]{ctex}\n',
+}
